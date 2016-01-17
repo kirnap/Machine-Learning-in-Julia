e1f4ad4599b9f677fd3f95570eb052daa9bceb93
OmerKirnap <okirnap@ku.edu.tr>
2016-01-17 16:21:31 +0200

    Add weight initializer function

--- word_representations/helper_functions.jl
+++ word_representations/helper_functions.jl
@@ -34,3 +34,40 @@ function modified_reshape(matrix; row_number=0, column_number=0)
       return reshape(matrix, row_number, column_number)
   end
 end
+
+
+function network_initializer(init_wt, numhid1, numhid2, numwords, vocab_size)
+  #=
+  This function is designed to create initial weights and delta matrices
+  for backpropagation algorithm
+  =#
+  word_embedding_weights = init_wt * randn(vocab_size, numhid1)
+  embed_to_hid_weights = init_wt * randn(numwords * numhid1, numhid2)
+  hid_to_output_weigths = init_wt * randn(numhid2, vocab_size)
+  hid_bias = zeros(numhid2, 1)
+  output_bias = zeros(vocab_size, 1)
+
+  word_embedding_weights_delta = zeros(vocab_size, numhid1)
+  word_embedding_weights_gradient = zeros(vocab_size, numhid1)
+
+  embed_to_hid_weights_delta = zeros(numwords * numhid1, numhid2)
+
+  hid_to_output_weights_delta = zeros(numhid2, vocab_size)
+
+  hid_bias_delta = zeros(numhid2, 1)
+
+  output_bias_delta = zeros(vocab_size, 1)
+
+return (word_embedding_weights,
+        embed_to_hid_weights,
+        hid_to_output_weigths,
+        hid_bias,
+        output_bias,
+        word_embedding_weights_delta,
+        word_embedding_weights_gradient,
+        embed_to_hid_weights_delta,
+        hid_to_output_weights_delta,
+        hid_bias_delta,
+        output_bias_delta)
+
+end
